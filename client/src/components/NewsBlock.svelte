d0dadf475a66a42e67f0e91b463724940ae71a93
Radames Ajna <radamajna@gmail.com>
2022-02-13 22:56:13 -0800

    styling feed

---
+++ client/src/components/NewsBlock.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let feedEntry;
+</script>
+
+<div class="group flex">
+	{#if feedEntry.media_content}
+		<a target="_blank" href={feedEntry.link}>
+			<img src={feedEntry.media_content[0].url} />
+		</a>
+	{/if}
+    <b>{feedEntry.sentiment.toFixed(3)}</b>
+
+	<a target="_blank" href={feedEntry.link}>
+		<h2>{feedEntry.title}</h2>
+		<h4>{feedEntry.author}</h4>
+		<p>{feedEntry.summary}</p>
+		{#if feedEntry.tags}
+			{#each feedEntry.tags as tag}
+				<span>{tag.term}</span>
+			{/each}
+		{/if}
+	</a>
+</div>
--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,20 +1,42 @@
 <script>
-	const fecthPredictions = fetch('news').then((d) => d.json());
+	import NewsBlock from '../components/NewsBlock.svelte';
+
+	let predictions;
+	let positiveOrder = true;
+	async function fecthPredictions() {
+		try {
+			predictions = await fetch('news').then((d) => d.json());
+		} catch (e) {
+			predictions = await fetch('static/test.json').then((d) => d.json());
+		}
+		predictions = predictions.sort((a, b) => b.sentiment - a.sentiment);
+		console.log(predictions);
+	}
+
+	function toggleOrder() {
+		positiveOrder = !positiveOrder;
+		predictions = predictions
+			.slice()
+			.sort((a, b) => (positiveOrder ? b.sentiment - a.sentiment : a.sentiment - b.sentiment));
+	}
 </script>
 
-<h1 class="text-3xl font-bold">
-	The New York Times Homepage
-</h1>
+<h1 class="text-3xl font-bold">The New York Times Homepage</h1>
 
-{#await fecthPredictions}
+<button
+	class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+	on:click={toggleOrder}
+>
+	Click me
+</button>
+
+{#await fecthPredictions()}
 	<p>Loading and running sentiment analysis on the latest news...</p>
 {:then data}
 	<ul>
-		{#each data as entry, i}
+		{#each predictions as entry, i}
 			<li>
-				<a target="_blank" href={entry.link}>
-					{i + 1}: {entry.title}
-				</a>
+				<NewsBlock feedEntry={entry} />
 			</li>
 		{/each}
 	</ul>
