b23f1a7885d5630fee9d1a09c1029585adbdaaa2
Radames Ajna <radamajna@gmail.com>
2022-02-15 16:18:44 -0800

    remove quotes

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,7 +1,20 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            'code::before': {
+              content: '""'
+            },
+            'code::after': {
+              content: '""'
+            }
+          }
+        }
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography')]
 };
